d20eaaed487c6aa5c6bb0edb3e47f2069d494790
Duks31 <chidubemjan31@gmail.com>
2025-04-22 21:39:41 +0100

    added models

--- rest_rag/chat/models.py
+++ rest_rag/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Conversation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    conversation =  models.ForeignKey(Conversation, on_delete= models.CASCADE, related_name="messages")
+    sender = models.CharField(max_length=10, choices = (("user", "User"), ("ai", "AI")))
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- rest_rag/documents/models.py
+++ rest_rag/documents/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class UploadedDocuments(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    file = models.FileField(upload_to="documents")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    document_name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.document_name or self.file.name
